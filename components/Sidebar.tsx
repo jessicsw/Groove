d9e1c5af867900c34d885bec281f0d7b57769f7c
jessicsw <jessicaselinewong@gmail.com>
2023-01-19 22:29:17 -0800

    Added nav and music menu buttons in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from "react-icons/md";
+import { Divider } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -26,25 +27,36 @@ const navMenu = [
   },
 ];
 
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Liked Songs",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
+
 const Sidebar = () => {
   return (
-    <div className="h-[calc(100vh-100px)] w-full bg-black px-1 text-gray-400">
-      <div className="py-5">
-        <div className="w-full px-5">
-          <Image
-            className="inline"
-            src="/logo.svg"
-            alt="groove logo"
-            height="50"
-            width="50"
-          />
-          <span className="pl-5 text-white">gr o o ve</span>
-        </div>
+    <div className="h-[calc(100vh-100px)] w-full bg-black p-5 text-gray-400">
+      <div className="w-full pb-5">
+        <Image
+          className="inline"
+          src="/logo.svg"
+          alt="groove logo"
+          height="50"
+          width="50"
+        />
+        <span className="pl-5 text-white">gr o o ve</span>
       </div>
-      <ul className="mb-5 text-gray-400">
+      <ul className="mb-5 text-gray-300">
         {navMenu.map((menu) => {
           return (
-            <li className="mb-2 px-5 text-base" key={menu.name}>
+            <li className="mb-2" key={menu.name}>
               <Link href={menu.route} passHref>
                 <menu.icon className="mr-4 inline" /> {menu.name}
               </Link>
@@ -52,6 +64,18 @@ const Sidebar = () => {
           );
         })}
       </ul>
+      <ul className="mt-9 mb-4 text-gray-300">
+        {musicMenu.map((menu) => {
+          return (
+            <li className="mb-2" key={menu.name}>
+              <Link href={menu.route} passHref>
+                <menu.icon className="mr-4 inline" /> {menu.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Divider className="my-0 mx-auto bg-gray-700" />
     </div>
   );
 };
