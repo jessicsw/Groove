90334feabd47b6668cf1eb5c3dc05a243715ee7e
jessicsw <jessicaselinewong@gmail.com>
2023-01-24 15:14:47 -0800

    updated configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // purge: ["./**/*.html", "./**/*.{js, jsx, ts, tsx}"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./components/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 };
