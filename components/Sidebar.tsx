c5e28b3aa7f468314f52dd2349377d353bc41c16
jessicsw <jessicaselinewong@gmail.com>
2023-01-26 17:43:39 -0800

    refactored sidebar nav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,28 +1,11 @@
-import { MdHome, MdSearch, MdLibraryMusic } from "react-icons/md";
-
 import Image from "next/image";
 import Link from "next/link";
-import SidebarMenu from "./SidebarMenu";
-
-const navMenu = [
-  {
-    name: "Home",
-    icon: MdHome,
-    route: "/",
-  },
-  {
-    name: "Search",
-    icon: MdSearch,
-    route: "/search",
-  },
-  {
-    name: "Your Library",
-    icon: MdLibraryMusic,
-    route: "/library",
-  },
-];
+import { Divider } from "@mui/material";
+import { usePlaylist } from "../lib/hooks";
+import { navMenu, musicMenu } from "../lib/sidebarRoutes";
 
 const Sidebar = () => {
+  const { playlists } = usePlaylist();
   return (
     <div className="h-[calc(100vh-100px)] w-full bg-black p-5 text-gray-400">
       <div className="h-full">
@@ -34,20 +17,58 @@ const Sidebar = () => {
             height="50"
             width="50"
           />
-          <span className="pl-5 text-white">gr o o ve</span>
+          <span className="pl-5 text-white">groove</span>
         </div>
-        <ul className="mb-5 text-gray-300">
-          {navMenu.map((menu) => {
-            return (
-              <li className="mb-2" key={menu.name}>
-                <Link href={menu.route} passHref>
-                  <menu.icon className="mr-4 inline" /> {menu.name}
+        <div className="h-[200px]">
+          <ul className="mb-9 text-gray-300">
+            {navMenu.map((menu) => {
+              return (
+                <li className="mb-2" key={menu.name}>
+                  <Link href={menu.route} passHref>
+                    <menu.icon className="mr-4 inline" /> {menu.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="mb-4 text-gray-300">
+            {musicMenu.map((menu) => {
+              return (
+                <li className="mb-2" key={menu.name}>
+                  <Link href={menu.route} passHref>
+                    <menu.icon className="mr-4 inline" /> {menu.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <Divider className="my-0 mx-auto bg-gray-700" />
+        </div>
+        <div className="h-[calc(100vh-390px)] overflow-y-auto">
+          <ul className="">
+            {playlists.map((playlist) => (
+              <li className="py-1 text-sm" key={playlist.id}>
+                <Link
+                  href={{
+                    pathname: "/playlist/[id]",
+                    query: { id: playlist.id },
+                  }}
+                  passHref
+                >
+                  {playlist.name}
                 </Link>
               </li>
-            );
-          })}
-        </ul>
-        <SidebarMenu />
+            ))}
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
---
+++ lib/sidebarRoutes.ts
@@ -0,0 +1,38 @@
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+export const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
+
+export const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Liked Songs",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
